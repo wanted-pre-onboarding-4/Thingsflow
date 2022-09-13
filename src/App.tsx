ff931915324cd9690c5b0abe2f294a390ee21bb7
Jaehyeok <milkboy2564@naver.com>
2022-09-13 20:56:11 +0900

    setting: 디렉토리 구조 및 라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route element={<div>Layout</div>}>
+          <Route path="/" element={<div>목록 페이지</div>}></Route>
+          <Route path="/detail/:issue_id" element={<div>상세 페이지</div>}></Route>
+        </Route>
+        <Route path="*" element={<div>404</div>}></Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;

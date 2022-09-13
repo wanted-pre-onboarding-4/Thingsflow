40492ed17a9f98b322251a088abc66a6b3f24055
Jaehyeok <milkboy2564@naver.com>
2022-09-13 22:47:55 +0900

    feat: Header Component Header 컴포넌트 완성

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <Container>
+      <HeaderTitle>Angular / Angular-cli</HeaderTitle>
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  height: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const HeaderTitle = styled.span`
+  color: ${({ theme }) => theme.GRAY_1};
+  font-size: 1.2rem;
+`;
 
 export default Header;

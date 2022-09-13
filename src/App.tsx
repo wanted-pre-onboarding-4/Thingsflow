b4ad53f5993482088f3457f8ad37493489905bb7
Jaehyeok <milkboy2564@naver.com>
2022-09-13 21:09:30 +0900

    feat: Layout 구조 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import GlobalStyle from './styles/Globalstyle';
+
 function App() {
   return (
     <Router>
+      <GlobalStyle />
       <Routes>
-        <Route element={<div>Layout</div>}>
+        <Route element={<Layout />}>
           <Route path="/" element={<div>목록 페이지</div>}></Route>
           <Route path="/detail/:issue_id" element={<div>상세 페이지</div>}></Route>
         </Route>

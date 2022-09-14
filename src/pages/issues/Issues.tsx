5c3d8c2cd28cad39eb57ca5d72b14d0ca49a8003
BoSeon Park <bedurgi999@gmail.com>
2022-09-14 15:46:09 +0900

    feat:issue 리스트, 무한스크롤, 광고 카드 추가

--- src/pages/issues/Issues.tsx
+++ src/pages/issues/Issues.tsx
@@ -1,22 +1,85 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect, useCallback, useRef } from 'react';
 import axios from 'axios';
-// import styled from 'styled-components';
+import IssueCard from './components/IssueCard';
+import styled from 'styled-components';
+
+type UserType = {
+  login: string;
+};
+
+export type IssuesType = {
+  number: number;
+  title: string;
+  author_association: string;
+  created_at: string;
+  body: string;
+  comments: number;
+  user: UserType;
+  id: number;
+};
 
 function Issues() {
-  const [issues, setissues] = useState<object[]>([]);
-  useEffect(() => {
-    const getIssues = async () => {
-      const { data } = await axios.get(`${process.env.REACT_APP_BASE_URL}issues`, {
-        headers: {
-          Authorization: `auth ${process.env.REACT_APP_ACCESS_TOKEN}`,
-        },
-      });
-      setissues(data);
-    };
-    getIssues();
+  const [issues, setissues] = useState<IssuesType[]>([]);
+  const target = useRef<HTMLDivElement>(null);
+  const [hasNextPage, setHasNextPage] = useState<boolean>(true);
+  const page = useRef<number>(1);
+
+  const fetch = useCallback(async () => {
+    try {
+      const { data } = await axios.get(
+        `${process.env.REACT_APP_BASE_URL}issues?per_page=7&&sort=comments&&page=${page.current}`,
+        {
+          headers: {
+            Authorization: `auth ${process.env.REACT_APP_ACCESS_TOKEN}`,
+          },
+        }
+      );
+      setissues(prevIssues => [...prevIssues, ...data]);
+      setHasNextPage(data.length === 7);
+      if (data.length) {
+        page.current += 1;
+      }
+    } catch (err) {
+      console.error(err);
+    }
   }, []);
+
+  useEffect(() => {
+    if (!target.current || !hasNextPage) return;
+
+    const io = new IntersectionObserver((entries, observer) => {
+      if (entries[0].isIntersecting) {
+        fetch();
+      }
+    });
+    io.observe(target.current);
+
+    return () => {
+      io.disconnect();
+    };
+  }, [fetch, hasNextPage]);
   console.log(issues);
-  return <h1>issues</h1>;
+  return (
+    <>
+      {issues.map((issue, idx) => (
+        <>
+          <IssueCard key={idx} issue={issue}></IssueCard>
+          {idx === 3 && (
+            <a href="https://thingsflow.com/ko/home">
+              <ADImg key={idx + issue.id} src={`${process.env.PUBLIC_URL}/logo.png`}></ADImg>
+            </a>
+          )}
+        </>
+      ))}
+      <div ref={target}></div>
+    </>
+  );
 }
+
+const ADImg = styled.img`
+  width: 768px;
+  height: 130px;
+  border-bottom: 1px solid black;
+`;
 
 export default Issues;
--- src/pages/issues/components/IssueCard.tsx
+++ src/pages/issues/components/IssueCard.tsx
@@ -1,7 +1,59 @@
 import React from 'react';
+import styled from 'styled-components';
+import { IssuesType } from '../Issues';
 
-function IssueCard() {
-  return <h1>123</h1>;
+type IssueProps = {
+  issue: IssuesType;
+};
+
+function IssueCard({ issue }: IssueProps) {
+  const dateToYMD = (date: string) => {
+    return `${date.slice(0, 4)}년 ${date.slice(5, 7)}월 ${date.slice(8, 10)}일`;
+  };
+
+  return (
+    <CardContainer>
+      <CardContent>
+        <CardTitle>{`# ${issue.number} ${issue.title}`}</CardTitle>
+        <CardCreateInfo>{`작성자: ${issue.user.login}, 작성일: ${dateToYMD(
+          issue.created_at
+        )}`}</CardCreateInfo>
+      </CardContent>
+      <CardComment>{`코멘트: ${issue.comments}`}</CardComment>
+    </CardContainer>
+  );
 }
+
+const CardContainer = styled.div`
+  width: 768px;
+  height: 130px;
+  display: flex;
+  border-bottom: 1px solid black;
+`;
+
+const CardTitle = styled.h1`
+  width: 600px;
+  height: 65px;
+  font-size: 20px;
+  font-weight: bold;
+  display: flex;
+  align-items: center;
+`;
+const CardContent = styled.div``;
+const CardCreateInfo = styled.p`
+  width: 600px;
+  height: 65px;
+  font-size: 18px;
+  display: flex;
+  align-items: center;
+`;
+
+const CardComment = styled.p`
+  width: 168px;
+  height: 130px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 export default IssueCard;

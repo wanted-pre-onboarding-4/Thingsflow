ee975dd76d42fac2229d3ab802ff6bafaf4cf296
kyungbaa <cocoroto@naver.com>
2022-09-14 20:39:31 +0900

    feat: 레이아웃 및 이전페이지가기 반응형 레이아웃과 이전페이지로 가기

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { ThemeProvider } from 'styled-components';
 import GlobalStyles from './GlobalStyles';
 import Issue from './page/Issue/Issue';
 import IssueList from './page/IssueList/IssueList';
+import NotFound from './page/NotFound/NotFound';
 import theme from './styles/theme';
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
           <Route path="/" element={<Navigate to="/issueList" />} />
           <Route path="/issueList" element={<IssueList />} />
           <Route path="/issue/:id" element={<Issue />} />
+          <Route path="/*" element={<NotFound />} />
         </Routes>
       </ThemeProvider>
     </BrowserRouter>
